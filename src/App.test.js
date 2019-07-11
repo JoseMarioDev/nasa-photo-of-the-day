752670abf1e7bce094a821d6b597ddf4923053dc
Jose Montero Jr <josemariodev@gmail.com>
2019-07-11 16:45:02 -0500

    installed testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import renderer from 'react-test-renderer';
 import ReactDOM from 'react-dom';
 import App from './App';
 
@@ -6,4 +7,9 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('matches snapshot', () => {
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
