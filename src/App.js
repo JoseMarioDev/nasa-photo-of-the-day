ff3e711993aacbd2a747c3e9524ab444157c104e
Jose Montero Jr <josemariodev@gmail.com>
2019-07-11 16:08:58 -0500

    rendering photo, title, and explanation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from "react";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import './App.css';
 
+import axios from 'axios';
 function App() {
+  const [pic, setPic] = useState();
+  const [title, setTitle] = useState();
+  const [explanation, setExplanation] = useState();
+
+  const fetchPhoto = () => {
+    axios
+      .get(
+        'https://api.nasa.gov/planetary/apod?api_key=9DSJJ5TmE1Vs2hLKmfyecx26QDjlRckOdzaSiNqF',
+      )
+      .then(res => {
+        console.log(res.data);
+        setPic(res.data.url);
+        setTitle(res.data.title);
+        setExplanation(res.data.explanation);
+      })
+      .catch(err => console.log(err));
+  };
+
+  useEffect(() => {
+    fetchPhoto();
+  }, []);
+
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+    <div className='App'>
+      <div className='titleContainer'>{title}</div>
+      <div className='imgContainer'>
+        <img src={pic} alt='pic of the day' />
+      </div>
+      <div className='explanationContainer'>{explanation}</div>
     </div>
   );
 }
